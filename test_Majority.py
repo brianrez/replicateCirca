b989aa23a39a8a4ebf70ad042ebcf76ed69e2adc
MohammadHossein Rezaei <118566365+brianrez@users.noreply.github.com>
2023-02-27 00:47:38 -0700

    Test the majority baseline

---
+++ test_Majority.py
@@ -0,0 +1,40 @@
+from datasets import load_dataset
+
+
+def dataset_loader():
+    '''
+    This functions loads the Circa dataset
+    and splits it randomly in three parts of
+    train, dev, test with 60, 20, 20 percentage
+    returns a datasetdict 
+    '''
+    dataset = load_dataset("circa", split = 'train')
+
+    #filter the unknown data
+    dataset = dataset.filter(lambda example: 
+                                (example['goldstandard2']==0 or 
+                                example['goldstandard2']== 1 or
+                                example['goldstandard2']== 2 or 
+                                example['goldstandard2']== 3))
+    
+    train_testvalid = dataset.train_test_split(test_size=0.4, seed=42)
+    test_valid = train_testvalid['test'].train_test_split(test_size=0.5, seed=42)
+    train_testvalid['test'] = test_valid['test']
+    train_testvalid['valid'] = test_valid['train']
+
+    return train_testvalid
+
+dataset = dataset_loader()
+
+id2label = {0: "Yes", 1: "No", 2: "In the middle, neither yes nor no", 3: "Yes, subject to some conditions)"}
+
+all = 0
+correct = 0
+for line in dataset['test']:
+    if 'Yes' == id2label[line['goldstandard2']]:
+        correct += 1
+        all += 1
+    else:
+        all +=1 
+
+print('This model\'s accuracy is: ' + str(correct / all))
